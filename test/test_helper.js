104c8d7920e5f8f85dabe239e54bc726546de53e
pcornejo <pcornejo@opengets.cl>
2018-03-15 14:20:07 -0300

    Webpack, Babel and Test suit setup

---
+++ test/test_helper.js
@@ -0,0 +1,17 @@
+import jsdom from 'jsdom';
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+
+const doc = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
+const win = doc.window;
+
+global.document = doc;
+global.window = win;
+
+Object.keys(win).forEach((key) => {
+    if (!(key in global)) {
+        global[key] = win[key];
+    }
+});
+
+chai.use(chaiImmutable);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    module: {
+        loaders: [{
+            test:/\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel'
+        }]
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+}
