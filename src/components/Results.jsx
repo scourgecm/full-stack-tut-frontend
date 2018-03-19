c6df2f563abe7e602748983ebe2654e71d0bc4c9
pcornejo <pcornejo@opengets.cl>
2018-03-19 11:55:12 -0300

    - UI completed - TODO: Create tally component and refactor into Results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
+import Winner from "./Winner";
 
 class Results extends React.PureComponent {
     render() {
-        return (
+        return this.props.winner ?
+            <Winner ref="winner" winner={this.props.winner} /> :
             <div className="results">
-                {this.getPair().map(entry =>
-                    <div key={entry} className="entry">
-                        <h1>{entry}</h1>
-                        <div className="voteCount">
-                            {this.getVotes(entry)}
+                <div className="tally">
+                    {this.getPair().map(entry =>
+                        <div key={entry} className="entry">
+                            <h1>{entry}</h1>
+                            <div className="voteCount">
+                                {this.getVotes(entry)}
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )}
+                </div>
+                <div className="management">
+                    <button className="next" ref="next" onClick={this.props.next}>
+                        Next
+                    </button>
+                </div>
             </div>
-        );
     }
     getPair() {
         return this.props.pair || [];
     }
     getVotes(entry) {
-        if (this.props.tally && this.props.tally.has(entry)){
+        if (this.props.tally && this.props.tally.has(entry)) {
             return this.props.tally.get(entry);
         }
         return 0;
