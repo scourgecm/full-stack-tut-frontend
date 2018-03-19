5c88051a6bb5fe35624ad3e0797b63fa487b28c6
root <root@Patricio.localdomain>
2018-03-18 23:05:45 -0300

    - Results components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { List } from "immutable";
+import { List, Map } from "immutable";
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({'Trainspotting': 5, '28 Days Later': 3});
 
 class App extends React.Component {
-    render(){
-        return React.cloneElement(this.props.children, {pair: pair});
+    render() {
+        return React.cloneElement(this.props.children, { pair, tally });
     }
 }
 
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
-class Results extends React.Component {
+class Results extends React.PureComponent {
     render() {
-      return (
-        <div>
-          Hello form Results!
-        </div>
-      )
+        return (
+            <div className="results">
+                {this.getPair().map(entry =>
+                    <div key={entry} className="entry">
+                        <h1>{entry}</h1>
+                        <div className="voteCount">
+                            {this.getVotes(entry)}
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
+    getPair() {
+        return this.props.pair || [];
+    }
+    getVotes(entry) {
+        if (this.props.tally && this.props.tally.has(entry)){
+            return this.props.tally.get(entry);
+        }
+        return 0;
     }
 }
 
